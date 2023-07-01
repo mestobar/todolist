d5995c9dd8acdd8b8d204c1aca1230a49e799a76
mestobar <mestobar@gmail.com>
2023-06-30 21:31:38 -0400

    Base de datos remoto

--- todolist/settings.py
+++ todolist/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+  #  DATABASES = {
+   #     'default': {
+    #        'ENGINE': 'django.db.backends.sqlite3',
+    #        'NAME': BASE_DIR / 'db.sqlite3',
+    #    }
+    #}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mestobar_todolist',
+        'USER': 'mestobar_test',
+        'PASSWORD': 'maxius1969',
+        'HOST': 'postgresql-mestobar.alwaysdata.net',
+        'PORT': '5432',
+
     }
 }
 
